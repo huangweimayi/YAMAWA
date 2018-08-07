7ca2eec82c85ddc4d72d8ed8fd32acdec48df91f
huangweimayi <793024079@qq.com>
2018-08-07 21:49:42 +0800

    121212

--- YAMAWA1/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // BASE_API: '"http://211.149.169.201/index.php/index"',
-  BASE_API: '"http://127.0.0.1/index.php/index"',
-})
